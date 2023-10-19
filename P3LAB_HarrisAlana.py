d350d0c6880d0bd7eb761745b3fa9e8d5c48c337
alana1977 <146122541+alana1977@users.noreply.github.com>
2023-10-19 14:29:11 -0400

    Working with nested if else statements

---
+++ P3LAB_HarrisAlana.py
@@ -0,0 +1,27 @@
+#Alana Harris
+#10/19/23
+#Working with nested if else statements
+
+#Create boolean variable to hold leap year status
+is_leap = False
+
+#Get year from user
+year = int(input("Enter a year to determine if it's a leap year: "))
+
+
+if year % 4 == 0:           #Does year divide by 4
+    if year % 100 == 0:     #Does year divide by 100
+        if year % 400 == 0: #Does year divide by 400
+            is_leap = True
+        else:
+            is_leap = False #Does NOT divide by 400
+    else:
+        is_leap = True      #Does NOT divide by 100
+else:
+    is_leap = False         #Does NOT divide by 4
+
+#Print output to user
+if is_leap == True:
+    print(f"The year {year} is a leap year")
+else:
+    print(f"The year {year} is NOT leap year")
